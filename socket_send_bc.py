9f2ffddc92d62a4c8c643f0b83bb7e6cf65c46eb
alealclag <alealclag@alum.us.es>
2021-04-04 18:51:55 +0200

    Actualizado send prueba

--- socket_send_bc.py
+++ socket_send_bc.py
@@ -1,19 +1,12 @@
 import socket
-import sys
 import time
 
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+server.settimeout(0.2)
 
-def broadcast(frequency, port):
-
-    server = socket.socket(
-        socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    server.settimeout(0.2)
-
-    while True:
-        server.sendto(b"prueba", ('<broadcast>', port))
-        print("message sent...")
-        time.sleep(frequency)
-
-
-broadcast(5, 1000)
+while True:
+    server.sendto(b"prueba", ('<broadcast>', 1000))
+    print("message sent...")
+    time.sleep(1)
