12190ac9b5b39af1182cb4bbf17ea7c0f8662a08
alealclag <alealclag@alum.us.es>
2021-04-05 02:16:05 +0200

    Descarte de mensajes propios

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import socket
 import RPi.GPIO as GPIO
 import json
-socket.gethostname()
+import os
+
+# Carga de la información del vehículo
 info = json.load(open("{}.json".format(socket.gethostname())))
+myip = os.popen("ip -4 route show default").read().split()[8]
 
 
+# Callback de la interrupción, que manda en broadcast la señal de disculpa
 def sendSorry(channel):
     print("message sent")
-    coche1.sendto(bytes("Sorry! by {} {} ({})".format(
+    sock.sendto(bytes("Sorry! by {} {} ({})".format(
         info["manufacturer"], info["model"], info["color"]), encoding='utf8'),
         ('192.168.1.255', 1050))
 
 
+# Configuración de la interrupción en GPIO
 sButton = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(sButton, GPIO.FALLING,
                       callback=sendSorry, bouncetime=1000)
 
-coche1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-coche1.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-coche1.bind(('', 1050))
+# Configuración del socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock.bind(('', 1050))
 # coche1.settimeout(0.2)
+
+
+# Bucle para la recepción de señales
 while True:
     print("Receiving...")
-    message, sender = coche1.recvfrom(1024)
-    print(message.decode("utf-8"))
+    message, sender = sock.recvfrom(1024)
+    senderIp = str(sender).split("'")[1]
+    if senderIp != myip:
+        print(message.decode("utf-8"))
