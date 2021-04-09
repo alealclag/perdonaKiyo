eb0e70956b879b7ae84ada12060efa5b6aa651ee
alealclag <alealclag@alum.us.es>
2021-04-09 23:48:25 +0200

    Sustuida Ip del remitente por la matrícula, y añadido timeout none

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import os
 
 # Carga de la información del vehículo
 info = json.load(open("{}.json".format(socket.gethostname())))
+myPlate = info["plate"]
 myip = os.popen("ip -4 route show default").read().split()[8]
 
 
 # Callback de la interrupción, que manda en broadcast la señal de disculpa
 def sendSorry(channel):
     print("message sent")
-    sock.sendto(bytes("Sorry! by {} {} ({})".format(
-        info["manufacturer"], info["model"], info["color"]), encoding='utf8'),
+    sock.sendto(bytes("Sorry! by {} {} ({}) /{}".format(
+        info["manufacturer"], info["model"], info["color"], info["plate"]), encoding='utf8'),
         ('192.168.1.255', 1050))
 
 
@@ -27,13 +28,14 @@ GPIO.add_event_detect(sButton, GPIO.FALLING,
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sock.bind(('', 1050))
-# coche1.settimeout(0.2)
+sock.settimeout(None)
 
 
 # Bucle para la recepción de señales
 while True:
     print("Receiving...")
     message, sender = sock.recvfrom(1024)
-    senderIp = str(sender).split("'")[1]
-    if senderIp != myip:
+    # = str(sender).split("'")[1]
+    senderPlate = str(message).split("/")[1]
+    if senderPlate != myPlate:
         print(message.decode("utf-8"))
